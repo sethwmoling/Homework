d787d668929a914f17e60595e11eb05573bcbec1
Seth Moling <sethwmoling@gmail.com>
2022-06-18 19:29:24 -0500

    week2homework

---
+++ week2.js
@@ -0,0 +1,7 @@
+let pizzaPlace = "Nate's Pizza"
+let numberOfToppings = 12
+console.log(typeof pizzaPlace)
+console.log(typeof numberOfToppings)
+let greeting = "Welcome to ${pizzaPlace} where we offer ${numberoftoppings} toppings!"
+if (numberOfToppings > 10)
+  console.log("A whole lot of pizza.")
