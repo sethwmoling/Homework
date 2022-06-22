7fac53e7bea078f17e2137829ddb2e8d58599d6f
Seth Moling <107004763+sethwmoling@users.noreply.github.com>
2022-06-22 16:28:37 -0500

    Update week2HomeWork.js

--- week2HomeWork.js
+++ week2HomeWork.js
@@ -3,5 +3,7 @@ let numberOfToppings = 12;
 console.log(typeof pizzaPlace);
 console.log(typeof numberOfToppings);
 let greeting = "Welcome to ${pizzaPlace} where we offer ${numberoftoppings} toppings!";
-if (numberOfToppings < 10);
+if (numberOfToppings >= 10){
   console.log("A whole lot of pizza.");
+}else {
+  console.log("I must be misunderstanding something.")}
